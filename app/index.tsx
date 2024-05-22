ec303b302288d0d66f9fbc7d9e0554ade813245c
Mark Lawlor <mwlawlor@gmail.com>
2024-05-22 09:32:27 +0200

    startup fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,9 @@
-import { View } from "react-native";
-import { Link } from "expo-router";
-import { useTrending } from "@/lib/hooks/useTrendingMedia";
-import MediaCarousel from "@/lib/components/MediaCarousel";
-import { SignIn } from "@/lib/components/SignIn";
+import { View, Text } from "react-native";
 
 export default function App() {
-  const trending = useTrending("movie");
   return (
     <View className="flex-1">
-      <MediaCarousel
-        data={trending}
-        title={<Link href="/movies">Movies</Link>}
-      />
-      <Link href="/sign-in">Sign In</Link>
+      <Text>Welcome to the Expo Router Workshop</Text>
     </View>
   );
 }
--- app/sign-in.tsx
+++
@@ -1,5 +0,0 @@
-import { SignIn } from "@/lib/components/SignIn";
-
-export default function SignInPage() {
-  return <SignIn />;
-}
